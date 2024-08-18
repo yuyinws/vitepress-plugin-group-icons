d3b474793c5599c8326ea521675e63325605999c
yuyinws <lyc657508366@gmail.com>
2024-08-18 21:56:30 +0800

    chore: one plugin

--- src/node/vite.ts
+++ src/node/vite.ts
@@ -1,7 +1,7 @@
 import type { Plugin, ViteDevServer } from 'vite'
 import { generateCSS } from './codegen'
 
-export function vitePluginGroupIcons(): Plugin[] {
+export function vitePluginGroupIcons(): Plugin {
   const virtualCssId = 'virtual:group-icons.css'
   const resolvedVirtualCssId = `\0${virtualCssId}`
   const labels = new Set<string>()
@@ -9,52 +9,49 @@ export function vitePluginGroupIcons(): Plugin[] {
   let server: ViteDevServer | undefined
   let cssContent = ''
 
-  return [
-    {
-      name: 'vitepress-plugin-group-icons',
+  return {
+    name: 'vitepress-plugin-group-icons',
+    resolveId(id) {
+      if (id === virtualCssId) {
+        return resolvedVirtualCssId
+      }
+      return undefined
+    },
 
-      resolveId(id) {
-        if (id === virtualCssId) {
-          return resolvedVirtualCssId
+    configureServer(_server) {
+      server = _server
+    },
+
+    async load(id) {
+      if (id === resolvedVirtualCssId) {
+        const { css } = await generateCSS(labels)
+        cssContent = css
+
+        return cssContent
+      }
+
+      return undefined
+    },
+    transform(code, id) {
+      if (id.endsWith('.md')) {
+        while (true) {
+          const match = labelRegex.exec(code)
+          if (!match)
+            break
+          labels.add(match[1])
         }
-        return undefined
-      },
-
-      configureServer(_server) {
-        server = _server
-      },
-
-      async load(id) {
-        if (id === resolvedVirtualCssId) {
+      }
+    },
+    transformIndexHtml: {
+      async handler() {
+        const mod = server!.moduleGraph.getModuleById(resolvedVirtualCssId)
+        if (mod) {
           const { css } = await generateCSS(labels)
           cssContent = css
-
-          return cssContent
+          server!.moduleGraph.invalidateModule(mod)
+          server!.reloadModule(mod)
         }
-
-        return undefined
-      },
-      transform(code, id) {
-        if (id.endsWith('.md')) {
-          while (true) {
-            const match = labelRegex.exec(code)
-            if (!match)
-              break
-            labels.add(match[1] || match[2])
-          }
-        }
-      },
-      transformIndexHtml: {
-        async handler() {
-          const mod = server!.moduleGraph.getModuleById(resolvedVirtualCssId)
-          if (mod) {
-            const { css } = await generateCSS(labels)
-            cssContent = css
-            server!.moduleGraph.invalidateModule(mod)
-            server!.reloadModule(mod)
-          }
-        },
       },
     },
-  ]
+  }
 }
